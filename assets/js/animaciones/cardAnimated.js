27ec47155824cfebe396e2f9de388a5d1d23a1bd
viviana <dmpviviana@gmail.com>
2018-10-14 02:42:34 -0300

    nuevo estilo de portafolio

---
+++ assets/js/animaciones/cardAnimated.js
@@ -0,0 +1,38 @@
+// (function() {
+//     var $guts = false;
+    
+//     $('.card').hover( function() {
+//       if (!$guts) {
+//          $guts = $(this).find('.card-guts');
+//       }
+//       $guts.velocity('stop').velocity({rotateY: '180deg'});
+//     }, function() {
+//        $guts.velocity('stop').velocity({rotateY: '0deg'});
+//     });
+    
+  
+//   })();
+
+
+  {/* <div class="wrap">
+  
+  <div class="card">
+    
+   1 <div class="card-guts">
+     2 <div class="card-front">
+        <div class="card-liner">
+        <h3>FrontSide</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, provident iste, ex odit explicabo minus quisquam non.</p>
+        </div>
+      2</div>
+      3<div class="card-back">
+        <div class="card-liner">
+        <h3>BackSide</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, provident iste, ex odit explicabo minus quisquam non.</p>
+        </div>
+      3</div>
+   1 </div>
+    
+  </div>
+  
+</div> */}
